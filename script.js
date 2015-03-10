98d34a1bc63beffc1158ecbbf69e78d83880d707
Jairo villalobos <breakjai@yahoo.com.mx>
2015-03-09 23:09:49 -0600

    agregando js top inicio

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
  
 
- (function($){
+//  (function($){
 
-	"use strict";
+// 	"use strict";
 
-	$(function(){
+// 	$(function(){
+
+// 		$(document).ready(function() {
+//    			alert("jQuery esta funcionando !!");
+//  				});
+
+// 	});
+
+
+// });
+
+ $(document).ready(function() {
+
+ 	$('#irinicio').click(function () {
+     $('html, body').animate({ scrollTop: '0px'}, 1000);
+     return false;
+   });
+   
+ });
 
 
 
-	});
 
 
-});
+
+ 
